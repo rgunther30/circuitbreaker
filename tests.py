4a29d9ce532062ca435e0d86068b313e50d739d7
Russ Gunther <rgunther30@gmail.com>
2016-05-21 16:07:40 -0400

    Add unit tests for state transitions

---
+++ tests.py
@@ -0,0 +1,53 @@
+import unittest
+import time
+
+import circuit_breaker
+
+
+DEFAULT_FAILS = 3
+DEFAULT_RETRY = 1
+
+
+class TestBreaker(unittest.TestCase):
+    def setUp(self):
+        self.breaker = circuit_breaker.CircuitBreaker(
+            allowed_fails=DEFAULT_FAILS,
+            retry_time=DEFAULT_RETRY,
+            validation_func=None
+        )
+        self.breaker_with_validation = circuit_breaker.CircuitBreaker(
+            allowed_fails=DEFAULT_FAILS,
+            retry_time=DEFAULT_RETRY
+        )
+
+    def test_open_transition(self):
+        breaker = self.breaker
+        for i in range(DEFAULT_FAILS):
+            breaker.on_failure()
+        self.assertEqual(breaker.state, circuit_breaker.OPEN)
+        self.assertEqual(breaker.failure_count, DEFAULT_FAILS)
+
+    def test_success(self):
+        breaker = self.breaker
+        for i in range(DEFAULT_FAILS - 1):
+            breaker.on_failure()
+        self.assertEqual(breaker.state, circuit_breaker.CLOSED)
+        self.assertEqual(breaker.failure_count, DEFAULT_FAILS - 1)
+
+        breaker.on_success()
+        self.assertEqual(breaker.state, circuit_breaker.CLOSED)
+        self.assertEqual(breaker.failure_count, 0)
+
+    def test_half_open(self):
+        breaker = self.breaker
+        for i in range(DEFAULT_FAILS):
+            breaker.on_failure()
+        self.assertEqual(breaker.state, circuit_breaker.OPEN)
+
+        time.sleep(DEFAULT_RETRY)
+        breaker.check_state()
+        self.assertEqual(breaker.state, circuit_breaker.HALF_OPEN)
+
+
+if __name__ == '__main__':
+    unittest.main()
